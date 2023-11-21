0f87be0172ee5720fe52e545d9d6eafa5b7eb33d
ant <rl2842001@gmail.com>
2023-11-20 23:02:27 -0500

    delete_quote

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .route("/quotes", post(handlers::create_quote))
         .route("/quotes", get(handlers::read_quotes))
         .route("/quotes/:id", put(handlers::update_quote))
+        .route("/quotes/:id", delete(handlers::delete_quote))
         .with_state(pool);
 
     axum::Server::bind(&addr.parse().unwrap()).serve(app.into_make_service()).await.unwrap();
